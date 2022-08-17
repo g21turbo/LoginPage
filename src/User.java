9ecd7fb4a703b473e5ac49539f292d8d4701314f
User <dg21stew@gmail.com>
2022-08-17 12:10:15 -0700

    Initial commit

---
+++ src/User.java
@@ -0,0 +1,14 @@
+import java.nio.file.Paths;
+
+public class User {
+
+    public String name;
+    public String email;
+    public String phone;
+    public String address;
+    public String password;
+
+
+
+
+}
